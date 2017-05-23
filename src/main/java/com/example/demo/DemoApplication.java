3d701b30ae8891ac0d6dc8f82f58e8839d3da9cf
vishal <vishalagr2008@gmail.com>
2017-05-23 14:52:16 +0530

    first commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+    @RequestMapping("/")
+    public String welcome(Map<String, Object> model) {
+//        model.put("message", this.message);
+        return "Hello World";
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
